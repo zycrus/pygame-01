12fabcc94bdea8f95b142c95a731c8894705a519
Zycrus <phantom.bot.202@gmail.com>
2022-08-18 16:23:50 +0800

    Added a script for player functions

---
+++ player.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__(self, color, x, y, _width, _height):
+        self.color = color
+        self.x = x
+        self.y = y
+        self._width = _width
+        self._height = _height
+
+    def Move():
+        return
