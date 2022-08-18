822137401041a4d04380f83678bc487c1ec71d49
Zycrus <phantom.bot.202@gmail.com>
2022-08-18 19:39:21 +0800

    Fixed some inputs and added jump input

--- kb_handler.py
+++ kb_handler.py
@@ -1,4 +1,4 @@
-from main import pygame
+import pygame
 
 h_positive = [ord("d"), pygame.K_RIGHT]
 h_negative = [ord("a"), pygame.K_LEFT]
@@ -8,21 +8,25 @@ v_negative = [ord("s"), pygame.K_DOWN]
 h_dir = 0
 v_dir = 0
 
+def GetJumpKey():
+    keys = pygame.key.get_pressed()
+    return keys[pygame.K_SPACE]
+
 def GetHorizontalKey():
     keys = pygame.key.get_pressed()
-    if keys[h_positive]:
+    if keys[h_positive[0]] or keys[h_positive[1]]:
         h_dir = 1
-    elif keys[h_negative]:
+    elif keys[h_negative[0]] or keys[h_negative[1]]:
         h_dir = -1
     else:
         h_dir = 0
     return h_dir
 
-def GetVeritcalKey():
+def GetVerticalKey():
     keys = pygame.key.get_pressed()
-    if keys[v_positive]:
+    if keys[v_positive[0]] or keys[v_positive[1]]:
         v_dir = 1
-    elif keys[v_negative]:
+    elif keys[v_negative[0]] or keys[v_negative[1]]:
         v_dir = -1
     else:
         v_dir = 0
